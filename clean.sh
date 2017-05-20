e2b4cf350180bf11eeb875dcdc26d80fecc3aaf7
Nicolas BENY <nbeny@e2r7p21.42.fr>
2017-05-20 09:32:50 +0200

    rm

--- clean.sh
+++
@@ -1,4 +0,0 @@
-#/bin/rm
-rm -f **/**.c\~
-rm -f **/**.h\~
-rm -f **/**Makefile\~
--- ft_printf/libft/src/ft_lstadd.c
+++
@@ -1,7 +0,0 @@
-#include "../include/libft.h"
-
-void	ft_lstadd(t_lib **alst, t_lib *new)
-{
-	new->next = *alst;
-	*alst = new;
-}
