0583f1a5e57a13ab7f13da647dce4b86f70aa18c
Nicolas BENY <nbeny@e2r7p21.42.fr>
2017-05-20 10:52:05 +0200

    clear

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#/bin/rm
-rm -f **/**.c\~
-rm -f **/**.h\~
-rm -f **/**Makefile\~
-rm -f Makefile\~
-rm -f **/**.c\#
-rm -f **/**.h\#
-rm -f **/**Makefile\#
--- ft_printf/libft/src/ft_putnstr_fd.c
+++
@@ -1,8 +0,0 @@
-#include "../include/libft.h"
-
-int		ft_putnstr_fd(int fd, char *s, size_t n)
-{
-	if (s && fd >= 0 && fd <= 2)
-		write(fd, s, n);
-	return (1);
-}
